a8b902613fcec0a18f0d9cf2ad6a6844f59e4316
Itou86 <1057541056@qq.com>
2021-06-22 13:32:09 +0800

    实现CRUD,1.0版本结束开发

--- src/main/java/com/itou86/bookmarket/controller/BookController.java
+++ src/main/java/com/itou86/bookmarket/controller/BookController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -32,6 +33,46 @@ public class BookController {
         List<Books> list = bookService.selectAllBook();
         model.addAttribute("list", list);
         return "allBook";
+    }
+
+    //跳转到新增书籍页面
+    @RequestMapping("/toAddBook")
+    public String toAddBook() {
+        return "addBook";
+    }
+
+    //新增书籍
+    @RequestMapping("/addBook")
+    public String addBook(Books books) {
+        System.out.println("新增" + books);
+        bookService.addBook(books);
+        return "redirect:/book/allBook"; //重定向到请求
+    }
+
+    //跳转到修改书籍界面
+    @RequestMapping("/toUpdateBook")
+    public String toUpdateBook(Model model, int id) {
+        Books books = bookService.selectBookById(id);
+        System.out.println("选择" + books);
+        model.addAttribute("book",books );
+        return "updateBook";
+    }
+
+    //修改书籍
+    @RequestMapping("/updateBook")
+    public String updateBook(Model model, Books book) {
+        System.out.println("修改为" + book);
+        bookService.updateBook(book);
+        Books books = bookService.selectBookById(book.getBookId());
+        model.addAttribute("books", books);
+        return "redirect:/book/allBook"; //重定向到请求
+    }
+
+    //删除书籍
+    @RequestMapping("/del/{bookId}")
+    public String deleteBook(@PathVariable("bookId") int id) {
+        bookService.deleteBookById(id);
+        return "redirect:/book/allBook";
     }
 
 }
---
+++ web/WEB-INF/jsp/updateBook.jsp
@@ -0,0 +1,37 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: itou86
+  Date: 2021/6/22
+  Time: 13:20
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <title>修改信息</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- 引入 Bootstrap -->
+    <link href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    <div class="row clearfix">
+        <div class="col-md-12 column">
+            <div class="page-header">
+                <h1>
+                    <small>修改信息</small>
+                </h1>
+            </div>
+        </div>
+    </div>
+    <form action="${pageContext.request.contextPath}/book/updateBook" method="post">
+        <input type="hidden" name="bookId" value="${book.bookId}"/>
+        书籍名称：<input type="text" name="bookName" value="${book.bookName}"/>
+        书籍数量：<input type="text" name="bookCounts" value="${book.bookCounts}"/>
+        书籍详情：<input type="text" name="bookDetail" value="${book.bookDetail}"/>
+        <input type="submit" value="提交"/>
+    </form>
+</div>
+</body>
+</html>
